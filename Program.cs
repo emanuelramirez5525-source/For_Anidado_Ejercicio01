1493003a0a9f776aa3718bc927da52cdf2486583
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 11:26:34 -0600

    logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿// Mostrar un cuadrado de 5x5 con números 1.
+for (int fila = 1; fila <=5; fila++)
+{
+    for (int columna =1; columna <=5; columna++)
+    {
+        Console.WriteLine("1");
+    }
+    Console.WriteLine();
+}
